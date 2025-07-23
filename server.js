8975ce3eabddd3c650686e01a8c1950b61d9ce92
kunaldevelopers <kunalking01grd@gmail.com>
2025-07-24 01:07:41 +0530

    initial commit2

--- server.js
+++ server.js
@@ -117,6 +117,7 @@ app.post("/verify-payment", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server started at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
 });
